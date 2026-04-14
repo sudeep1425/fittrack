e50f5feb695a43096f4d37b9a8604f1e4008117f
sudeep1425 <sudeepreddyg71@gmail.com>
2026-04-14 11:52:57 +0530

    axois

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// Fallback if env is missing
 const baseURL =
   import.meta.env.VITE_API_URL || "https://fittrack-a0zu.onrender.com";
 
@@ -8,7 +7,6 @@ const api = axios.create({
   baseURL: `${baseURL}/api`,
 });
 
-// Attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
